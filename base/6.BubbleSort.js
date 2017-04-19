7e2acf18990060e3836598490427cad02e7ff808
Liang-xiao <marsgt@qq.com:qqpqpq19860403>
2017-04-19 20:18:59 +0800

    每日更新

--- base/6.BubbleSort.js
+++ base/6.BubbleSort.js
@@ -4,10 +4,25 @@
  */
 
 /**
- * 简易冒泡排序
+ * 冒泡排序
  */
 (function () {
     function bubbleSort(arr) {
-        // TODO:
+        var i,j,temp;
+        var len = arr.length;
+        for (i = 0; i < len; i++) {
+            for (j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
     }
+    // testing
+    var a = bubbleSort([8, 100, 50, 22, 15, 6, 1, 1000, 999, 0]);
+    console.log(a);
+    // 由于有循环嵌套，所以冒泡的时间复杂度为O(N^2)
 })();
---
+++ base/7.QuickSort.js
@@ -0,0 +1,13 @@
+/**
+ * @file 数据结构和算法的基础练习 · 冒泡排序
+ * @author 熊猫桑
+ */
+
+/**
+ * 快速排序
+ */
+(function(){
+    function quickSort() {
+        //TODO:
+    }
+})();
